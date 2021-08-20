15b716fb53ff04bec5dac5b6a363bfa76ff4e8cc
Swathi <swathi.h@perfios.com>
2021-08-20 20:19:45 +0530

    Die roll to get a random number

--- src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -1,10 +1,16 @@
 package com.Bridgelabz.SnakeAndLadder;
 
 public class SnakeAndLadder {
+	public static int rollDice() {
+		int outcome = (int) Math.floor(Math.random() * 6 + 1);
+		return outcome;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Snake and Ladder Game *****");
 		int win = 100;
 		int start = 0;
 		int score = 0;
+		System.out.println(rollDice());
 	}
 }
