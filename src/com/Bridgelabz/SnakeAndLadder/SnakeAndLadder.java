5b65acd510ac36831afd6f70ee5872bdd39330cd
Swathi <swathi.h@perfios.com>
2021-08-20 17:46:13 +0530

    Initial Commit

---
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.Bridgelabz.SnakeAndLadder;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		System.out.println("***** Welcome to Snake and Ladder Game *****");
+	}
+}
