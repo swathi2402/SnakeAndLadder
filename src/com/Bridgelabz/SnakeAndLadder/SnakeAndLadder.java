e74268609f6f230ccbb46600dd3cff47154a5720
Swathi <swathi.h@perfios.com>
2021-08-20 19:45:32 +0530

    ADDED: Class SnakeAndLadder where play stats at position 0

--- src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -3,5 +3,8 @@ package com.Bridgelabz.SnakeAndLadder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Snake and Ladder Game *****");
+		int win = 100;
+		int start = 0;
+		int score = 0;
 	}
 }
