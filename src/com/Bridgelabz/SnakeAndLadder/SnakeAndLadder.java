e15cff8a103f49be0383eadcb2778834da5a3b1d
Swathi <swathi.h@perfios.com>
2021-08-20 20:23:23 +0530

    ADDED: Method options to check no play, snake and ladder

--- src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -5,12 +5,18 @@ public class SnakeAndLadder {
 		int outcome = (int) Math.floor(Math.random() * 6 + 1);
 		return outcome;
 	}
+	
+	public static int options() {
+		int option = (int) Math.floor(Math.random() * 3);
+		return option;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Snake and Ladder Game *****");
 		int win = 100;
 		int start = 0;
 		int score = 0;
-		System.out.println(rollDice());
+//		System.out.println(rollDice());
+		System.out.println(options());
 	}
 }
