905fa387037dd63a8566695c319cd35afd62a69a
Swathi <swathi.h@perfios.com>
2021-08-20 22:02:34 +0530

    Adding comments

--- src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
+++ src/com/Bridgelabz/SnakeAndLadder/SnakeAndLadder.java
@@ -6,12 +6,14 @@ public class SnakeAndLadder {
 	public static final int SNAKE = 2;
 	public static final int WIN = 100;
 	public static final int START = 0;
-	
+
+//	Rolling dice using random
 	public static int rollDice() {
 		int outcome = (int) Math.floor(Math.random() * 6 + 1);
 		return outcome;
 	}
 
+//	Selecting options on No paly, ladder, snake
 	public static int options() {
 		int option = (int) Math.floor(Math.random() * 3);
 		return option;
@@ -19,17 +21,19 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("***** Welcome to Snake and Ladder Game *****");
+//		Variable defining
 		int player1 = 0;
 		int player2 = 0;
 		int options = 0;
 		int diceValue = 0;
 		boolean isPlayer1 = true;
-		
+
 		while (player1 <= WIN || player2 <= WIN) {
 			diceValue = rollDice();
 			options = options();
 
 			switch (options) {
+//			Doing nothing, passing to other player
 			case NO_PLAY:
 				if (isPlayer1) {
 					isPlayer1 = false;
@@ -38,14 +42,14 @@ public class SnakeAndLadder {
 				}
 				break;
 
+//			Moving farward, one more chance if it is ladder based on flag isPlayer
 			case LADDER:
 				if (isPlayer1) {
 					if (player1 + diceValue > WIN) {
 						diceValue = 0;
 					}
 					player1 += diceValue;
-				}
-				else {
+				} else {
 					if (player2 + diceValue > WIN) {
 						diceValue = 0;
 					}
@@ -53,16 +57,16 @@ public class SnakeAndLadder {
 				}
 				break;
 
+//			Moving backward
 			case SNAKE:
-				if(isPlayer1) {
+				if (isPlayer1) {
 					if (player1 - diceValue < START) {
 						player1 = 0;
 					} else {
 						player1 -= diceValue;
 					}
 					isPlayer1 = false;
-				} 
-				else {
+				} else {
 					if (player2 - diceValue < START) {
 						player2 = 0;
 					} else {
@@ -70,13 +74,16 @@ public class SnakeAndLadder {
 					}
 					isPlayer1 = true;
 				}
+				break;
 			}
+
 			System.out.println("Position:	" + "Player 1: " + player1 + " Player 2:  " + player2);
+			
 			if (player1 == WIN) {
 				System.out.println("Player 1 won the game");
 				break;
-			}
-			else if (player2 == WIN) {
+			} 
+			if (player2 == WIN) {
 				System.out.println("Player 2 won the game");
 				break;
 			}
